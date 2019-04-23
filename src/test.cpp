eab88306072c5fab8fe29d0e6d776ee7f905c0b0
Guilherme Cardoso <gchicoper@gmail.com>
2019-04-23 16:40:02 -0300

    comments

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,8 @@
 extern "C"
 {
-    volatile unsigned int * const UART0DR = (unsigned int *)0x10009000;
+    volatile unsigned int * const UART0DR = (unsigned int *)0x10009000; // base memory address of the console output (UART0)
     
+    // outputs to UART0
     void print_uart0(const char *s) {
         while(*s != '\0') { /* Loop until end of string */
             *UART0DR = (unsigned int)(*s); /* Transmit char */
@@ -9,6 +10,7 @@ extern "C"
         }
     }
 
+    // our "main" function
     void c_entry() {
         print_uart0("Isto esta rodando em metal desnudo\n");
     }
